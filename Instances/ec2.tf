6490abc5cbf3479df5b15d03867997333723de0c
sandeepyeluri2107 <145672763+sandeepyeluri2107@users.noreply.github.com>
2023-11-30 20:21:43 +0000

    Update ec2.tf

--- Instances/ec2.tf
+++ Instances/ec2.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "myec2" {
-  ami                      = "ami-id"
+  ami                      = "ami-0fc5d935ebf8bc3bc"
   instance_type     = "t2.micro"
   count                   = "3"
 }
